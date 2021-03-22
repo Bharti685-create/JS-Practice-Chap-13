394b1b102110e233f24484f89d2c7c694cc3df08
Bharti Shinde <Bharti.Shinde01@student.csulb.edu>
2021-03-22 12:43:09 -0700

    Chapter13

---
+++ js/ex1.js
@@ -0,0 +1,19 @@
+// Show a DOM object's child node
+// "node" is the DOM object
+// "index" is the index of the child node
+const showChild = (node, index) => {
+  const nodeElements = node.childNodes;
+  console.log(nodeElements[index]);
+ 
+
+// Should show the h1 node
+showChild(document.body, 1);
+
+// Should show "Incorrect index"
+showChild(document.body, -1);
+
+// Should show "Incorrect index"
+showChild(document.body, 8);
+
+// Should show "Wrong node type"
+showChild(document.body.childNodes[0], 0);
